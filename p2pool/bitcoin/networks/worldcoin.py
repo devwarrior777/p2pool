6108e37ad3afe08322f69e7dfaca3ca963553d1a
CartmanSPC <CartmanSPC@users.noreply.github.com>
2016-03-07 10:39:02 -0800

    Update worldcoin.py
    ...to account for the constantly lowering subsidy. Needs to be lower that the current subsidy or there is a rare chance of this causing an orphan block.

--- p2pool/bitcoin/networks/worldcoin.py
+++ p2pool/bitcoin/networks/worldcoin.py
@@ -15,7 +15,7 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
             'worldcoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         ))
-SUBSIDY_FUNC = lambda height: 30*100000000
+SUBSIDY_FUNC = lambda height: 20*100000000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 30 # s
 SYMBOL = 'WDC'
